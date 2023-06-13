4f22c08df81e45a7dcd1be5f1d312503b4de40cf
AlexMikhalev <alex@metacortex.engineer>
2023-06-13 11:00:58 +0100

    Debug startup script for pipeline

--- service/start-pipeline.sh
+++ service/start-pipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo $(hostname -I) redisgraph | sudo tee -a /etc/hosts
 echo $(hostname -I) rgcluster | sudo tee -a /etc/hosts
-bash /code/terraphim-platform-pipeline/cluster_pipeline_stream_prod.sh
+cd /code/terraphim-platform-pipeline/ && bash /code/terraphim-platform-pipeline/cluster_pipeline_stream_prod.sh
