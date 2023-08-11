ba6f9ff6f8e206297273521537de3df154a9556b
AlexMikhalev <alex@metacortex.engineer>
2023-08-11 12:39:12 +0100

    Added role of systems operator
    Signed-off-by: AlexMikhalev <alex@metacortex.engineer>

--- cluster_pipeline_batch.sh
+++ cluster_pipeline_batch.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
-python3 parse_publish_dates.py
+# python3 parse_publish_dates.py
 # set debug to true - comment it out on fresh cluster or set value to 0 to disable debug, by defaul pipelines logs everything - every query 
 gears-cli run --host 127.0.0.1 --port 30001 set_debug_key.py
 gears-cli run --host 127.0.0.1 --port 30001 gears_pipeline_sentence_register.py --requirements requirements_gears_pipeline.txt
 echo "NLP Pipeline registered."
 gears-cli run --host 127.0.0.1 --port 30001 edges_to_graph_streamed.py --requirements requirements_gears_graph.txt
 echo "edges_to_graph_streamed.py registered"
-# echo "10 seconds for cluster to recover"
-# sleep 10
+echo "10 seconds for cluster to recover"
+sleep 10
+gears-cli run --host 127.0.0.1 --port 30001 sentences_matcher_run_operator.py --requirements requirements_all.txt
 # gears-cli run --host 127.0.0.1 --port 30001 sentences_matcher_register_medical.py --requirements requirements_gears_aho.txt
 # echo "sentences_matcher_register.py registered."
 # gears-cli run --host 127.0.0.1 --port 30001 sentences_matcher_run_project.py --requirements requirements_gears_aho.txt
